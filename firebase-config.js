e29250bf7a70981f5a625cd21e2ea415463b4fbf
GustavoDGoat <gustavodgoat@gmail.com>
2025-07-20 09:16:58 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDgV-0uGzndNh7Kcfyxu_GwHdbLGPPsG0k",
-  authDomain: "purge-extension.firebaseapp.com",
-  projectId: "purge-extension",
-  storageBucket: "purge-extension.firebasestorage.app",
-  messagingSenderId: "983930555373",
-  appId: "1:983930555373:web:dd8f020142cbe4c6c875e4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
